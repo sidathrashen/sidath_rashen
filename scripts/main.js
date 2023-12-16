c9191c47c7b8e7183364a1e33378794f4670ca00
pamodiranmal <ranmalmendis8@gmail.com>
2023-12-16 05:46:11 +0530

    updated

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 // Add your javascript here
 // Don't forget to add it into respective layouts where this js file is needed
 
+
 $(document).ready(function() {
   AOS.init( {
     // uncomment below for on-scroll animations to played only once
@@ -8,30 +9,47 @@ $(document).ready(function() {
   }); // initialize animate on scroll library
 
   document.getElementById('downloadCvButton').addEventListener('click', function() {
-    const { jsPDF } = window.jspdf;
-    html2canvas(document.body).then(canvas => {
-      const pdf = new jsPDF('p', 'mm', 'a4');
-      const imgData = canvas.toDataURL('image/png');
-      const imgWidth = 210;
-      const pageHeight = 297;  
-      const imgHeight = canvas.height * imgWidth / canvas.width;
-      let heightLeft = imgHeight;
-      let position = 0;
+             // Create a new anchor element
+             const a = document.createElement('a');
 
-      pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
-      heightLeft -= pageHeight;
+             // Set the href attribute to the file's URL
+             a.href = 'ranmal_mendis_cv.pdf'; // Replace with the actual path to the file
+ 
+             // Set the download attribute to the desired file name
+             a.download = 'ranmal_mendis/ranmal_mendis_cv.pdf';
+ 
+             // Append the anchor to the body
+             document.body.appendChild(a);
+ 
+             // Trigger a click on the anchor
+             a.click();
+ 
+             // Remove the anchor from the body
+             document.body.removeChild(a);
+  //   const { jsPDF } = window.jspdf;
+  //   html2canvas(document.body).then(canvas => {
+  //     const pdf = new jsPDF('p', 'mm', 'a4');
+  //     const imgData = canvas.toDataURL('image/png');
+  //     const imgWidth = 210;
+  //     const pageHeight = 297;  
+  //     const imgHeight = canvas.height * imgWidth / canvas.width;
+  //     let heightLeft = imgHeight;
+  //     let position = 0;
 
-      while (heightLeft >= 0) {
-          position = heightLeft - imgHeight;
-          pdf.addPage();
-          pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
-          heightLeft -= pageHeight;
-      }
+  //     pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+  //     heightLeft -= pageHeight;
 
-      pdf.save('cv.pdf');
-  }).catch(error => {
-      console.error('Error: ', error);
-  });
+  //     while (heightLeft >= 0) {
+  //         position = heightLeft - imgHeight;
+  //         pdf.addPage();
+  //         pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+  //         heightLeft -= pageHeight;
+  //     }
+
+  //     pdf.save('ranmal_mendis_cv.pdf');
+  // }).catch(error => {
+  //     console.error('Error: ', error);
+  // });
 });
 
 
